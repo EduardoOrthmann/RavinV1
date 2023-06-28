f3b0b59e83871a500bbd0d3584eb1cf274004444
Eduardo <orthmann2811@gmail.com>
2023-06-28 20:33:02 -0300

    update payment class to work just with a service

--- src/payment/PaymentService.java
+++ src/payment/PaymentService.java
@@ -1,35 +1,6 @@
 package payment;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
 public class PaymentService {
-    private final PaymentDAO paymentDAO;
-
-    public PaymentService() {
-        this.paymentDAO = new PaymentDAO();
-    }
-
-    public Payment findById(int id) {
-        return paymentDAO.findById(id).orElseThrow(() -> new NoSuchElementException("Menu não encontrado"));
-    }
-
-    public List<Payment> findAll() {
-        return paymentDAO.findAll();
-    }
-
-    public void save(Payment entity) {
-        paymentDAO.save(entity);
-    }
-
-    public void update(Payment entity) {
-        paymentDAO.update(entity);
-    }
-
-    public void delete(Payment entity) {
-        paymentDAO.delete(entity);
-    }
-
     // TODO
     public double pay(double cash) {
         return 0;
